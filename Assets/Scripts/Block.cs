af2918a31104d7481ee43a1dbd0713824c8a35c7
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-04-22 00:51:04 -0700

    added collision trigger on block to destroy game object.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,18 @@
+
+/*
+
+CODY
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour 
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
